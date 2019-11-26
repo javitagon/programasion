7f4e2e9696ae224527797771198f9a998a0410e2
javi <Javitama53@gmail.com>
2019-11-26 20:01:44 +0100

    commit primero

---
+++ Ej34/src/principal/Ej34.java
@@ -0,0 +1,71 @@
+package principal;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Ej34 {
+
+	public static void main(String[] args) {
+		Scanner reader= new Scanner(System.in);
+		int menu=0;	
+		System.out.println("Definir alfabeto.  ");
+		System.out.println("Cesar ");
+		System.out.println("Monoalfabético general ");
+		System.out.println("Escitalo ");
+		System.out.println("Vigénere ");
+		System.out.println("ADFGVX ");
+		System.out.println("Salir ");
+		System.out.println("Escoja una opción (1-7): ");
+		switch(menu) {
+		case 1:{
+		String Abecedario= ("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ 123456789,;.<=>-_()+*/!+?%:abcdefghijklmnopqrstuvwxyz");
+		System.out.println("tu abecedario actual es"+(Abecedario));
+		boolean select= false;
+		System.out.println("quieres cambiarlo? ");
+		String Siono= reader.nextLine();
+		if (Siono == "si");
+		else if (Siono=="no");
+		Abecedario=reader.nextLine();	
+		}
+			break;
+		case 2:{
+			
+			
+			
+		}
+			break;
+		case 3:{
+		
+			
+			
+		}
+			break;
+		case 4:{
+			
+			
+			
+		}
+			break;
+		case 5:{
+			
+			
+			
+		}
+			break;
+		case 6:{
+		
+			
+			
+		}
+			break;
+		case 7:{
+			
+			
+			
+		}
+			break;
+	
+	
+	
+		}
+	}
+
+}
